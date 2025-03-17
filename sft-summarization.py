0c1000f1d97a859fda11017c4ef4f7cd817d2302
Aidan Do <aidando73@gmail.com>
2025-03-17 06:12:01 +0000

    .

--- sft-summarization.py
+++ sft-summarization.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[6]:
+# In[1]:
 
 
 import datasets
@@ -12,7 +12,7 @@ dataset = datasets.load_dataset("trl-lib/tldr")
 dataset['train'][0]
 
 
-# In[7]:
+# In[2]:
 
 
 from transformers import AutoTokenizer, AutoModelForCausalLM
@@ -28,37 +28,89 @@ model = AutoModelForCausalLM.from_pretrained(
 )
 
 
-# In[ ]:
+# In[3]:
+
+
+# Calculate token lengths for the dataset
+def to_chat_template(prompt, completion):
+    return tokenizer.apply_chat_template([
+        {"role": "system", "content": prompt},
+        {"role": "assistant", "content": completion},
+    ], tokenize=False)
+
+# print("Calculating token lengths...")
+# def get_token_lengths(rows):
+#     prompts = rows["prompt"]
+#     completions = rows["completion"]
+    
+#     # Tokenize prompts and completions
+#     total_lengths = [tokenizer(to_chat_template(prompt, completion), return_length=True)["length"] for prompt, completion in zip(prompts, completions)]
+    
+#     return {"total_lengths": total_lengths}
+
+# # Map function to get token lengths
+# token_lengths = dataset.map(get_token_lengths, batched=True, batch_size=100)
+
+# # Calculate statistics
+# total_lengths = token_lengths["train"]["total_lengths"]
+
+# # Calculate max and 95th percentile
+# max_total_length = max(total_lengths)
+# p95_total_length = int(torch.tensor(total_lengths, dtype=torch.float32).quantile(0.95).item())
+
+# print(f"Max token lengths: total={max_total_length}")
+# print(f"P95 token lengths: total={p95_total_length}")
+
+
+# In[4]:
 
 
 print("Converting training dataset to tokens...")
 def tokenize_chat(rows):
-    chats = [[
-        {"role": "system", "content": prompt},
-        {"role": "assistant", "content": completion},
-    ] for prompt, completion in zip(rows["prompt"], rows["completion"])]
-    chat_text = [
-        tokenizer.apply_chat_template(chat, tokenize=False)
-        for chat in chats
-    ]
-    return tokenizer(chat_text, padding=True, truncation=True, max_length=1024, return_tensors="pt")
+    chats = [to_chat_template(prompt, completion) for prompt, completion in zip(rows["prompt"], rows["completion"])]
+
+    return tokenizer(
+        chats,
+        padding="max_length",
+        truncation=True,
+        # 618 is the max length of the dataset
+        max_length=618,
+        return_tensors="pt"
+    )
 
 tokenized_dataset = dataset.map(tokenize_chat, batched=True)
 
 
-# In[20]:
+# In[5]:
 
 
-from transformers import TrainingArguments, Trainer
+# Drop the original prompt and completion columns as they're no longer needed
+# after tokenization to save memory
+columns_to_remove = ["prompt", "completion", "attention_mask"]
+tokenized_dataset = tokenized_dataset.remove_columns(columns_to_remove)
+
+print(f"Columns after removal: {tokenized_dataset['train'].column_names}")
+
+
+# In[ ]:
+
+
+from transformers import TrainingArguments, Trainer, DataCollatorForLanguageModeling
 import wandb
 import os
 from dotenv import load_dotenv
+from datetime import datetime
 
 load_dotenv()
 
-output_dir = "Qwen2-0.5B-SFT"
+output_dir = f"Qwen2-0.5B-SFT-{datetime.now().strftime('%Y-%m-%d-%H-%M')}"
 
-wandb.login(os.environ["WANDB_API_KEY"])
+wandb.login(key=os.environ["WANDB_API_KEY"])
+
+data_collator = DataCollatorForLanguageModeling(
+    tokenizer=tokenizer,
+    mlm=False
+)
 
 training_args = TrainingArguments(
     output_dir=output_dir,
@@ -75,6 +127,7 @@ trainer = Trainer(
     args=training_args,
     train_dataset=tokenized_dataset['train'],
     eval_dataset=tokenized_dataset['validation'],
+    data_collator=data_collator,
 )
 
 trainer.train()
